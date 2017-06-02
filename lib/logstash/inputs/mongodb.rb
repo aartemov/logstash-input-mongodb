fd6ad341de4ae03c87f20bf3825208c9be507605
Juan Pablo Arias <jarias@thor.version3.co>
2017-06-02 16:03:23 -0500

    [FIX] structure of sqlite - Use String type for 'place' column in sqlite fix #38

--- lib/logstash/inputs/mongodb.rb
+++ lib/logstash/inputs/mongodb.rb
@@ -79,7 +79,7 @@ class LogStash::Inputs::MongoDB < LogStash::Inputs::Base
     begin
       sqlitedb.create_table "#{SINCE_TABLE}" do
         String :table
-        Int :place
+        String :place
       end
     rescue
       @logger.debug("since table already exists")
